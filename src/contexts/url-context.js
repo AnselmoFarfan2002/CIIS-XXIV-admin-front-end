fb632e60571cdab7561ea6d9731aa9ca6b8164a0
AnselmoFarfan2002 <anselmofarfan2002@gmail.com>
2023-08-09 12:57:14 -0500

    add qr scan
    new dependencies installed

--- src/contexts/url-context.js
+++ src/contexts/url-context.js
@@ -4,6 +4,7 @@ const URI = {
   sessions: domain + "/api/v1/sessions",
   registrations: domain + "/api/v1/registrations",
   users: domain + "/api/v1/users",
+  events: domain + "/api/v1/events",
 };
 
 export default URI;
--- src/pages/attendance.js
+++ src/pages/attendance.js
@@ -4,8 +4,11 @@ import {
   Card,
   CardContent,
   CardHeader,
+  Dialog,
+  DialogContent,
   Grid,
   InputBase,
+  Switch,
   TextField,
   Typography,
 } from "@mui/material";
@@ -15,15 +18,51 @@ import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
 import HowToRegIcon from "@mui/icons-material/HowToReg";
 import QrReader from "react-qr-scanner";
 import { useState } from "react";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import DangerousIcon from "@mui/icons-material/Dangerous";
+import URI from "src/contexts/url-context";
 
 const Page = () => {
   const [result, setResult] = useState(null);
+  const [scan, setScan] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [failure, setFailure] = useState(false);
 
   const handleScan = (result) => {
     if (result) {
       setResult(result);
-      console.log(result);
+      fetch(result.text)
+        .then((res) => {
+          if (res.ok) return setSuccess(true);
+          throw new Error();
+        })
+        .catch(() => setFailure(true));
     }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+
+    if (event.target.checkValidity()) {
+      let dni = event.target.querySelector("input").value;
+      fetch(URI.events + `/12/attendance?user=${dni}`)
+        .then((res) => {
+          if (res.ok) return setSuccess(true);
+          throw new Error();
+        })
+        .catch(() => setFailure(true));
+    }
+  };
+
+  const handleCloseSuccess = () => {
+    setSuccess(false);
+    setResult(null);
+  };
+
+  const handleCloseFailure = () => {
+    setFailure(false);
+    setResult(null);
   };
 
   return (
@@ -46,8 +85,7 @@ const Page = () => {
                 <Grid item md={4} xs={12}>
                   <Card>
                     <CardContent>
-                      <Box component={"form"}>
-                        {" "}
+                      <Box component={"form"} onSubmit={handleSubmit}>
                         <Box
                           sx={{
                             display: "flex",
@@ -66,10 +104,15 @@ const Page = () => {
                             name="dni"
                             label="Ingrese DNI"
                             fullWidth
+                            inputProps={{
+                              pattern: "^[0-9]{8}$", // Expresión regular
+                              title: "Ingrese un DNI válido",
+                            }}
                             required
                           />
                         </Box>
                         <Button
+                          type="submit"
                           variant="text"
                           sx={{
                             color: "grey",
@@ -97,13 +140,27 @@ const Page = () => {
                         }}
                       >
                         <Typography variant="h6">Marcar por scan QR</Typography>
+
+                        <Box>
+                          <Typography variant="title2">Habilitar escaner QR</Typography>
+                          <Switch
+                            inputProps={{ "aria-label": "Color switch demo" }}
+                            color="secondary"
+                            onChange={() => setScan(!scan)}
+                          />
+                        </Box>
                       </Box>
-                      <Box>
-                        {!result && (
+
+                      <Box display={"flex"} justifyContent={"center"}>
+                        {!result && scan ? (
                           <QrReader
+                            width={200}
+                            height={200}
                             onError={() => console.log("existe un error al escanear")}
                             onScan={handleScan}
                           />
+                        ) : (
+                          <Box minHeight={200} minWidth={200} backgroundColor="rgba(0,0,0,.1)" />
                         )}
                       </Box>
                     </CardContent>
@@ -114,6 +171,32 @@ const Page = () => {
           </Stack>
         </Container>
       </Box>
+
+      <Dialog open={success} onClose={handleCloseSuccess}>
+        <DialogContent>
+          <Container>
+            <Typography variant="h6" mb={2} textAlign={"center"}>
+              Asistencia marcada con éxito
+            </Typography>
+            <Typography align="center">
+              <VerifiedIcon sx={{ fontSize: 100 }} color="success" />
+            </Typography>
+          </Container>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={failure} onClose={handleCloseFailure}>
+        <DialogContent>
+          <Container>
+            <Typography variant="h6" mb={2} textAlign={"center"}>
+              Ocurrió un error durante la asistencia
+            </Typography>
+            <Typography align="center">
+              <DangerousIcon sx={{ fontSize: 100 }} color="error" />
+            </Typography>
+          </Container>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
