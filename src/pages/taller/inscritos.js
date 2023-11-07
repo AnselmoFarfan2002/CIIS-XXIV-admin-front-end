9166a51d85ddda358dccc523c774439171d582aa
AnselmoFarfan2002 <anselmofarfan2002@gmail.com>
2023-11-06 23:35:45 -0500

    2.1.1
    repair some bugs to export

--- src/pages/taller/inscritos.js
+++ src/pages/taller/inscritos.js
@@ -6,48 +6,45 @@ import { TallerTable } from "src/sections/taller/taller-table";
 import URI from "src/contexts/url-context";
 import { saveOnChest, takeFromChest } from "src/utils/chest";
 import { useRouter } from "next/router";
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 import { TablaInscritos } from "src/sections/taller/inscritos-table";
 
 const Page = () => {
+  let taller = takeFromChest("taller");
+  // console.log(taller);
 
-    let taller = takeFromChest("taller");
-    // console.log(taller);
-
-    return (
+  return (
     <>
-        <Head>
-            <title>Taller específico</title>
-        </Head>
-        <Box
-            component="main"
-            sx={{
-                flexGrow: 1,
-                py: 8,
-            }}
-        >
+      <Head>
+        <title>Taller específico</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
         <Container maxWidth="lg">
-            <Stack spacing={3}>
+          <Stack spacing={3}>
             <div>
-                <Typography variant="h4">Taller "{taller.name}"</Typography>
+              <Typography variant="h4">Taller {`"${taller.name}"`}</Typography>
             </div>
             <div>
-                <Grid xs={12} md={6} lg={8}>
-                    <TablaInscritos 
-                        primTaller = {taller}  
-                    />
-                </Grid>
+              <Grid xs={12} md={6} lg={8}>
+                <TablaInscritos primTaller={taller} />
+              </Grid>
             </div>
-            </Stack>
+          </Stack>
         </Container>
-        </Box>
+      </Box>
     </>
-    );
+  );
 };
 
 Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
